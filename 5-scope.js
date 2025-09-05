199812a1d4de2c5734cb91201d5fb75ab07dde0a
benspector-mls <ben@marcylabschool.org>
2025-09-05 11:28:25 -0400

    updating example

--- 5-scope.js
+++ 5-scope.js
@@ -1,17 +1,24 @@
-// friend can be referenced anywhere within this file
-const friend = 'John';
+// myName is module-scoped. Reachable anywhere in this file.
+const myName = 'John';
 
-const greetFriend = (myName) => {
-  // The parameter myName is considered a function-scoped variable
+const greetFriend = (friend) => {
+  // Parameters like `friend` are function-scoped. Reachable anywhere in this function.
 
-  // We can access the module-scoped variable `friend` here since we are in a lower scope.
-  const message = `Hi, ${friend}, I'm ${myName}`;
-
-  console.log(message);
+  if (!friend) {
+    // message is block-scoped. Reachable only in this block.
+    const message = "I can't say hi if I don't know your name!";
+    console.log(message);
+  } else {
+    const message = `Hi, ${friend}, I'm ${myName}`;
+    console.log(message);
+  }
 }
 
-greetFriend('Jane');
-// Output: "Hi, John, I'm Jane"
+greetFriend("");
+// Output: I can't say hi if I don't know your name!
 
-// We get ReferenceErrors if we try to access variables out of scope.
+greetFriend('Jane');
+// Output: "Hi, Jane, I'm John"
+
+// We get ReferenceErrors if we try to reference variables out of scope.
 console.log(message, myName);
