a2b32deeb6434a207a9ee5b2f157e73be9d1b750
benspector-mls <ben@marcylabschool.org>
2025-09-05 10:52:18 -0400

    updating example

--- 5-scope.js
+++ 5-scope.js
@@ -1,18 +1,17 @@
-// Global Scope: this value can be referenced in any file. DON'T DO THIS
-global.x = 10; 
+// friend can be referenced anywhere within this file
+const friend = 'John';
 
-// Module Scope: this value can only be referenced within this file
-const friend = 'John'; 
-
-// Function Scope: The myName parameter and message variable can only be referenced within greetFriend
 const greetFriend = (myName) => {
-  // We can access the module scoped `friend` here since we are in a lower scope.
+  // The parameter myName is considered a function-scoped variable
+
+  // We can access the module-scoped variable `friend` here since we are in a lower scope.
   const message = `Hi, ${friend}, I'm ${myName}`;
-  
+
   console.log(message);
 }
 
-greetFriend('Jane'); 
+greetFriend('Jane');
 // Output: "Hi, John, I'm Jane"
 
-console.log(message); // ReferenceError: message is not defined in this scope
+// We get ReferenceErrors if we try to access variables out of scope.
+console.log(message, myName);
